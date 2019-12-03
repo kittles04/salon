3c6448fdfd9b6ad2d7b2eeaf4b7caa4debd29418
kerri walker <kerriannwalker3@gmail.com>
2019-12-02 20:41:40 -0800

    Add CSS Grid to div container on Navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,13 +1,11 @@
 <template>
-  <div id="navigation">
-    <div id="main-nav">
+  <div id="container">
       <h2>Salon</h2>
-          <ul>
+          <ul id="container__ul">
             <li><router-link to="/">Home</router-link></li>
             <li><router-link to="/video">Video Page</router-link></li>
             <li><router-link to="/blog">Blog Page</router-link></li>
           </ul>
-    </div>
   </div>
 </template>
 
@@ -26,6 +24,9 @@ export default{
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss"scoped>
 @import './sass/_base.scss';
+@import './sass/_components.scss';
+@import './sass/_reset.scss';
+
 
 
 
