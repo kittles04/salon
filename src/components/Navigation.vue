3d7609eca9cc688937bc76c0240ee4c8e163f660
kerri walker <kerriannwalker3@gmail.com>
2019-12-03 18:58:48 -0800

    Placement of Elements in Nav Bar with CSS Grid

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,11 +1,21 @@
+// <template>
+//   <div id="container">
+//       <h2>Salon</h2>
+//           <ul id="container__ul">
+//             <li id="container--one"><router-link to="/" class="routerlink">Home</router-link></li>
+//             <li id="container--two"><router-link to="/video" class="routerlink">Video Page</router-link></li>
+//             <li id="container--three"><router-link to="/blog" class="routerlink">Blog Page</router-link></li>
+//           </ul>
+//   </div>
+// </template>
+
 <template>
   <div id="container">
       <h2>Salon</h2>
-          <ul id="container__ul">
-            <li id="container--one"><router-link to="/" class="routerlink">Home</router-link></li>
-            <li id="container--two"><router-link to="/video" class="routerlink">Video Page</router-link></li>
-            <li id="container--three"><router-link to="/blog" class="routerlink">Blog Page</router-link></li>
-          </ul>
+     
+           <router-link to="/" class="routerlink1">Home</router-link>
+           <router-link to="/video" class="routerlink2">Video Page</router-link>
+           <router-link to="/blog" class="routerlink3">Blog Page</router-link>
   </div>
 </template>
 
